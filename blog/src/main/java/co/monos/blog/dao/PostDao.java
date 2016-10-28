bb46b53c09271a90944be72946438657a0c57b58
jeseon <jeseon@gmail.com>
2016-10-28 22:05:49 +0900

    디렉토리 수정

--- blog/src/main/java/co/monos/blog/dao/PostDao.java
+++
@@ -1,9 +0,0 @@
-package co.monos.blog.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import co.monos.blog.domain.Post;
-
-public interface PostDao extends JpaRepository<Post, Integer> {
-
-}
