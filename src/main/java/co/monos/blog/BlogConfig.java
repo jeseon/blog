383ac0de277bad028300431e0479b466fa47e4ca
jeseon <jeseon@gmail.com>
2016-12-16 10:49:23 +0900

    H2 console 설정 application.yml에 적용

--- src/main/java/co/monos/blog/BlogConfig.java
+++ src/main/java/co/monos/blog/BlogConfig.java
@@ -1,21 +1,12 @@
 package co.monos.blog;
 
-import org.h2.server.web.WebServlet;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @Configuration
 public class BlogConfig {
-	@Bean
-	public ServletRegistrationBean h2servletRegistration() {
-		ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
-		registration.addUrlMappings("/console/*");
-		return registration;
-	}
-	
 	@Bean
 	public FilterRegistrationBean hiddenHttpMethodRegistration() {
 	    FilterRegistrationBean registration = new FilterRegistrationBean();
