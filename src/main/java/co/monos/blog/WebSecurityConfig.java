9e9dcae22f461ce75c17dbd8431cf82a2c745231
jeseon <jeseon@gmail.com>
2017-01-05 18:40:25 +0900

    로그인 기능 구현 (SpringSecurity)

---
+++ src/main/java/co/monos/blog/WebSecurityConfig.java
@@ -0,0 +1,38 @@
+package co.monos.blog;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import co.monos.blog.service.UserService;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private UserService userService;
+	
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .formLogin()
+                .loginPage("/user/login")
+                .loginProcessingUrl("/user/login")
+            .and()
+                .logout()
+                .logoutUrl("/user/logout")
+                .deleteCookies("JSESSIONID")
+                .logoutSuccessUrl("/posts")
+            .and()
+                .authorizeRequests()
+                .antMatchers("/**/new*", "/**/edit*", "/**/delete*").authenticated()
+                .antMatchers("/**").permitAll();
+    }
+    
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService);
+    }   
+}
